f6cc48c8898b144aac87c0fc8cf079dec02fa470
gerke01 <59712192+gerke01@users.noreply.github.com>
2021-11-20 12:07:43 -0500

    Add files via upload

--- NeighborlyAPI/getPosts/__init__.py
+++ NeighborlyAPI/getPosts/__init__.py
@@ -10,9 +10,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python getPosts trigger function processed a request.')
 
     try:
-        url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+        url = "mongodb://gerke01cosmosdb:eSo8d0yu2wsRg1WeaDjda6natdIbwwCezQ3D5t0ksJTADOJHnSli0maVKvL2DQpvSQVufY60aiWYxWqNQ684wQ==@gerke01cosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@gerke01cosmosdb@"  # TODO: Update with appropriate MongoDB connection information
         client = pymongo.MongoClient(url)
-        database = client['azure']
+        database = client['gerke01cosmosdb']
         collection = database['posts']
 
         result = collection.find({})
